a604014bbc8be3b7146f2b3f775253fe025ca3e5
lovro_micek <lovro.micek2@gmail.com>
2020-10-19 15:47:05 +0200

    Updated DZ1.

---
+++ DZ1/ClassLibrary1/Episode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Episode
+    {
+        int viewers;
+        double ratingSum;
+        double maxRating;
+
+        public Episode()
+        {
+            viewers = 0;
+            ratingSum = 0;
+            maxRating = 0;
+        }
+        
+        public Episode(int viewers, double ratingSum, double maxRating)
+        {
+            this.viewers = viewers;
+            this.ratingSum = ratingSum;
+            this.maxRating = maxRating;
+        }
+
+        public void AddView(double rating)
+        {
+            ratingSum += rating;
+            viewers += 1;
+            if (rating > maxRating)
+                maxRating = rating;
+        }
+
+        public double GetMaxScore()
+        {
+            return maxRating;
+        }
+
+        public double GetAverageScore()
+        {
+            return ratingSum / viewers;
+        }
+
+        public int GetViewerCount()
+        {
+            return viewers;
+        }
+    }
+}
--- DZ1/DZ1/Program.cs
+++ DZ1/DZ1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary1;
+using System;
 
 namespace DZ1
 {
@@ -6,7 +7,30 @@ namespace DZ1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+			double GenerateRandomScore()
+			{
+				double rnd;
+				Random random = new Random();
+				rnd = random.NextDouble() * 10;
+				return rnd;
+			}
+			Episode ep1, ep2;
+			ep1 = new Episode();
+			ep2 = new Episode(10, 64.39, 8.7);
+			int viewers = 10;
+			for (int i = 0; i < viewers; i++)
+			{
+				ep1.AddView(GenerateRandomScore());
+				Console.WriteLine(ep1.GetMaxScore());
+			}
+			if (ep1.GetAverageScore() > ep2.GetAverageScore())
+			{
+				Console.WriteLine($"Viewers: {ep1.GetViewerCount()}");
+			}
+			else
+			{
+				Console.WriteLine($"Viewers: {ep2.GetViewerCount()}");
+			}
+		}
     }
 }
